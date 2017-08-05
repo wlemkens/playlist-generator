9df215c69d05cbb4ec091644643f96a48e4ac1e6
Wim Lemkens <wim.lemkens@gmail.com>
2017-08-05 11:46:30 +0200

    Remove the debug info

--- DanceAudioTagger/DanceAudioTagger.py
+++ DanceAudioTagger/DanceAudioTagger.py
@@ -28,26 +28,20 @@ class DanceAudioTagger(object):
 			
 	def createTaggedSong(self,songfile,genreDict):
 		try:
-			print ("Loading file '"+songfile+"'")
 			filename = songfile[len(self.musicPath):]
 			if DirectoryTools.getFileType(songfile)=="mp3":
 				song = AudioSegment.from_mp3(songfile)
 			else:
 				song = AudioSegment.from_flac(songfile)
-			print ("Fetching genre")
 			genreType = DirectoryTools.getGenre(songfile)
-			print ("Getting genre file")
 			genrefile = genreDict[genreType]
-			print ("Loading genre '"+genrefile+"'")
 			genre = AudioSegment.from_mp3(genrefile)
-			print ("Generating tagged file")
 			pause = AudioSegment.silent(duration=10000)
 			taggedSong = genre+pause+genre+song
 			outfile = self.outputPath+filename
 			outPath = os.path.dirname(outfile)
 			if not os.path.exists(outPath):
 				os.makedirs(outPath)
-			print ("Exporting to '"+outfile+"'")
 			taggedSong.export(outfile,format="mp3")
 		except:
 			print ("Failed to prepend "+self.outputPath+filename)
