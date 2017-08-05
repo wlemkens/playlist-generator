a35ce826412a6f07fa42efa9f3025f6dbbd838de
Wim Lemkens <wim.lemkens@gmail.com>
2017-08-05 11:17:42 +0200

    Command line tagger is now working

--- DanceAudioTagger/DanceAudioTagger.py
+++ DanceAudioTagger/DanceAudioTagger.py
@@ -8,16 +8,16 @@ from Tools import DirectoryTools
 
 class DanceAudioTagger(object):
 	def __init__(self,musicPath,genrePath,outputPath):
-		self.musicPath = musicPath
-		self.genrePath = genrePath
-		self.outputPath = outputPath
+		self.musicPath = musicPath.rstrip("/")
+		self.genrePath = genrePath.rstrip("/")
+		self.outputPath = outputPath.rstrip("/")
 	
 	def tagMusic(self):
 		fileList = DirectoryTools.getFilesFromDirectory(self.musicPath)
 		genreList = DirectoryTools.getFilesFromDirectory(self.genrePath)
 		genreDict = self.getGenres(genreList)
 		for song in fileList:
-			createTaggedSong(song)
+			self.createTaggedSong(song,genreDict)
 	
 	def getGenres(self,genreList):
 		genreDict = {}
@@ -26,5 +26,28 @@ class DanceAudioTagger(object):
 			genreDict[genre] = genreName
 		return genreDict
 			
-	def createTaggedSong(self,song):
-		pass
+	def createTaggedSong(self,songfile,genreDict):
+		try:
+			print ("Loading file '"+songfile+"'")
+			filename = songfile[len(self.musicPath):]
+			if DirectoryTools.getFileType(songfile)=="mp3":
+				song = AudioSegment.from_mp3(songfile)
+			else:
+				song = AudioSegment.from_flac(songfile)
+			print ("Fetching genre")
+			genreType = DirectoryTools.getGenre(songfile)
+			print ("Getting genre file")
+			genrefile = genreDict[genreType]
+			print ("Loading genre '"+genrefile+"'")
+			genre = AudioSegment.from_mp3(genrefile)
+			print ("Generating tagged file")
+			pause = AudioSegment.silent(duration=10000)
+			taggedSong = genre+pause+genre+song
+			outfile = self.outputPath+filename
+			outPath = os.path.dirname(outfile)
+			if not os.path.exists(outPath):
+				os.makedirs(outPath)
+			print ("Exporting to '"+outfile+"'")
+			taggedSong.export(outfile,format="mp3")
+		except:
+			print ("Failed to prepend "+self.outputPath+filename)
