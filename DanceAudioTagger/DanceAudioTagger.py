19a6c0c38d7ad453081e87ca9449f4a92d64aac8
Wim Lemkens <wim@avoid.be>
2017-08-05 09:15:15 +0200

    Generating a list of the dances

--- DanceAudioTagger/DanceAudioTagger.py
+++ DanceAudioTagger/DanceAudioTagger.py
@@ -2,6 +2,7 @@
 
 # sudo pip install pydub
 from pydub import AudioSegment
+from Tools import DirectoryTools
 
 class DanceAudioTagger(object):
 	def __init__(self,musicPath,genrePath,outputPath):
@@ -10,4 +11,15 @@ class DanceAudioTagger(object):
 		self.outputPath = outputPath
 	
 	def tagMusic(self):
-		pass
+		fileList = DirectoryTools.getFilesFromDirectory(self.musicPath)
+		genreList = DirectoryTools.getFilesFromDirectory(self.genrePath)
+		genreDict = self.getGenres(genreList)
+		print (genreDict)
+	
+	def getGenres(self,genreList):
+		genreDict = {}
+		for genreName in genreList:
+			genre = genreName.split(".")[:-1].join("")
+			genreDict[genre] = genreName
+		return genreDict
+			
